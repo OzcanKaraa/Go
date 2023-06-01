e9a3ba87650f8b3761f2443ec6d9a2765116b032
Özcan Kara <ozcankara810@gmail.com>
2023-06-01 04:26:27 +0300

    some changes

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main //Paket tanımlama
 
-//Paket ekleme
-import (
-	"golesson/conditionals"
-)
+import "golesson/conditionals"
 
 func main() {
 	//variables.Demo1()
 	//fmt.Print()
-	conditionals.Demo1()
-
+	//conditionals.Demo1()
+	//conditionals.Demo2()
+	// conditionals.Demo3()
+	conditionals.Demo4()
 }
