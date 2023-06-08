8834beaf7a9370e6bf8aeac58eb4351f928c2dfc
Özcan Kara <ozcankara810@gmail.com>
2023-06-08 22:11:50 +0300

    some changes

---
+++ loops/workshop1.go
@@ -0,0 +1,12 @@
+package loops
+
+import "fmt"
+
+func Demo3() {
+	// aklimdakiSayi := 80
+	tahminEdilenSayi := 0
+
+	fmt.Println("Bir sayı tahmin tahmin ediniz?")
+	fmt.Scanln(&tahminEdilenSayi)
+	fmt.Println(tahminEdilenSayi)
+}
