34a9b1aa39643d0fb65abedb34ca153618f31c89
Özcan Kara <ozcankara810@gmail.com>
2023-06-15 23:27:27 +0300

    some changes

--- slices/demo1.go
+++ slices/demo1.go
@@ -10,6 +10,8 @@ func Demo1() {
 	isimler[0] = "Ozcan"
 	isimler[1] = "Derin"
 	isimler[2] = "Salih"
+	isimler = append(isimler, "Berfin") // Yeni Eleman ekleme
 
 	fmt.Println(isimler)
+	fmt.Println(len(isimler))
 }
