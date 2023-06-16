51ba7e4e57487347b35419cc941a5800d13ab033
Özcan Kara <ozcankara810@gmail.com>
2023-06-17 02:49:23 +0300

    some changes

--- slices/demo2.go
+++ slices/demo2.go
@@ -1,4 +1,26 @@
 package slices
 
+import "fmt"
+
 func Demo2() {
+	sehirler := []string{"Ankara", "İstanbul", "İzmir"}
+	fmt.Println(sehirler)
+
+	sehirlerKopya := make([]string, len(sehirler))
+	fmt.Println(sehirlerKopya)
+
+	copy(sehirlerKopya, sehirler)
+	fmt.Println(sehirlerKopya)
+
+	sehirler = append(sehirler, "Hatay")
+	fmt.Println(sehirler)
+	fmt.Println(sehirlerKopya)
+
+	fmt.Println(sehirler[1:3]) //Indis 1 den 3 e kadar dahil degil
+	fmt.Println(sehirler[1:5])
+
+	fmt.Println(sehirler[1:]) //Indis 1 den tumu
+
+	fmt.Println(sehirler[:2]) //indis 0 dan 2 ye kadar
+
 }
