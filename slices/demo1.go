014f1bc83027b7ce0d9dda18994d3b119d71e690
Özcan Kara <ozcankara810@gmail.com>
2023-06-17 02:53:03 +0300

    some changes

--- slices/demo1.go
+++ slices/demo1.go
@@ -2,15 +2,16 @@ package slices
 
 import "fmt"
 
+//Slice Gelismis Dizi
 func Demo1() {
-	isimler := make([]string, 3)
+	isimler := make([]string, 3) //make ile olusturma
 
 	fmt.Println(isimler)
 
 	isimler[0] = "Ozcan"
 	isimler[1] = "Derin"
 	isimler[2] = "Salih"
-	isimler = append(isimler, "Berfin") // Yeni Eleman ekleme
+	isimler = append(isimler, "Berfin") //append ile Yeni Eleman ekleme
 
 	fmt.Println(isimler)
 	fmt.Println(len(isimler))
--- slices/demo2.go
+++ slices/demo2.go
@@ -6,16 +6,17 @@ func Demo2() {
 	sehirler := []string{"Ankara", "İstanbul", "İzmir"}
 	fmt.Println(sehirler)
 
-	sehirlerKopya := make([]string, len(sehirler))
+	sehirlerKopya := make([]string, len(sehirler)) // MAKE ile yeni dizi olusturuldu.
 	fmt.Println(sehirlerKopya)
 
-	copy(sehirlerKopya, sehirler)
+	copy(sehirlerKopya, sehirler) //slice başka slice atama
 	fmt.Println(sehirlerKopya)
 
 	sehirler = append(sehirler, "Hatay")
 	fmt.Println(sehirler)
 	fmt.Println(sehirlerKopya)
 
+	//Slice Okuma
 	fmt.Println(sehirler[1:3]) //Indis 1 den 3 e kadar dahil degil
 	fmt.Println(sehirler[1:5])
 
