8eccaf78bce16c83115b8aff8ed55b153807efb4
Özcan Kara <ozcankara810@gmail.com>
2023-06-25 12:29:02 +0300

    some changes

--- structs/demo1.go
+++ structs/demo1.go
@@ -6,6 +6,7 @@ func Demo1() {
 	fmt.Println(product{"Laptop", 23000, "MacBook"})
 	fmt.Println(product{"Laptop", 23000, ""})
 	fmt.Println(product{name: "Laptop", unitPrice: 23000})
+	fmt.Println(product{name: "Laptop"})
 }
 
 type product struct {
