eb1ef303ace9df1077d3e05d6b39280984d014d9
Özcan Kara <ozcankara810@gmail.com>
2023-06-25 14:28:12 +0300

    some changes

--- channels/demo1.go
+++ channels/demo1.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-// channels :Asenkron çalışmasını ve yaşam döngüsünü kontrol eder
+// Channels :Asenkron çalışmasını ve yaşam döngüsünü kontrol eder
 func CiftSayilar(ciftSayiCn chan int) {
 	toplam := 0
 	for i := 0; i <= 10; i += 2 {
